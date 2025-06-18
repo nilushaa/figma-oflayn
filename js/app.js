97a3385895f6abe0a119f7bb6a158bb35a89116c
nilufar <nilufar@gmail.com>
2025-06-19 00:48:06 +0500

    .

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('form')
+const formButton = document.getElementById('form__button')
+const user = document.getElementById('user')
+const deleteBtn = document.getElementById('delete__btn')
+const clearBtn = document.getElementById('clear__button')
+
+
