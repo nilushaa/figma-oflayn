a0cefd5e7988f392912ca42664e7213865e28384
nilufar <nilufar@gmail.com>
2025-06-19 04:27:14 +0500

    .

--- js/request.js
+++ js/request.js
@@ -1,57 +1,53 @@
-
 // api
 const API = "https://randomuser.me/api/?results=9";
 
 // for leader
 const overlay = document.getElementById("overlay");
 
-
 //toggle loader
-const loaderToggle = (toggle) => {
-if (toggle) {
-    overlay.classList.remove('hidden')
-}else{
-    overlay.classList.add(hidden)
-}
-}
 
+const loaderToggle = (toggle) => {
+    if (toggle) {
+        overlay.classList.remove('hidden')
+    }else{
+        overlay.classList.add('hidden')
+    }
+}
 
 //request promise
 
 const getData = (resource) => {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
     request.addEventListener("readystatechange", () => {
-      if(request.readyState == 4) {
+     if (request.readyState < 4){
         loaderToggle(true)
-      }else if (request.readyState == 4 && request.status == 200){
-        const dota = JSON.parse(request.responseText)
-        resolve(data.results);
-        loaderToggle(false)
-      }else if (request.request == 4) {
-        reject(' Error!!!');
-        loaderToggle(false)
         
-      }
+     }else if (request.readyState == 4 && request.status == 200) {
+        const data =JSON.parse(request.responseText)
+        resolve(data.results); 
+        loaderToggle(false)
+     }else if (request.request == 4) {
+        reject('Error !!!');
+        loaderToggle(false)
+     }
     });
 
-request.open('GET', resource)
+    request.open("GET", resource);
     request.send();
   });
 };
 
-
-//lood
-
+//load
 const reload = () => {
     getData(API)
     .then((data) => {
-        console.log('then data:' , data);
+       updateUI(data);
         
-    }) .catch((err) => {
+    }).catch((err) => {
         console.log(err);
         
     })
 }
-document.addEventListener('DOMContentLoaded' , reload)
+
+document.addEventListener('DOMContentLoaded', reload)
