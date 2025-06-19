b1425ceded08299ea5ad1c91d1c490de4e6a745d
nilufar <nilufar@gmail.com>
2025-06-19 15:24:37 +0500

    .

--- js/app.js
+++ js/app.js
@@ -4,44 +4,43 @@ const user = document.getElementById("user");
 const deleteBtn = document.getElementById("delete__btn");
 const clearBtn = document.getElementById("clear__button");
 
+// refresh
+formButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  reload();
+  clearBtn.classList.remove("hidden");
+});
 
-//refresh
-formButton.addEventListener('clic', (e) => {
-e.preventDefault()
-reload()
-clearBtn.classList.remove('hidden')
-})
+// clear
+clearBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  user.innerHTML = "";
+  clearBtn.classList.add("hidden");
+});
 
-//clear 
-clearBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    user.innerHTML = ''
-    clearBtn.classList.add('hidden')
-} )
+// search by name
+form["form__input"].addEventListener("input", () => {
+  const inputValue = form["form__input"].value.toLocaleLowerCase();
+  const name = document.querySelectorAll(".user__name");
 
-//seach by name
-form['form__input'].addEventListener('input' , () => {
-    const inputValue = form['form__input'].value.toLocaleLowerCase()
-    const name =document.querySelectorAll('.user__name')
-   
-name.forEach((item) => {
-    if(item.lastElementChild.textContent.toLowerCase().includes(inputValue)) {
-        item.parentElement.classList.remove('hidden')
-    }else {
-        item.parentElement.classList.add('hidden')
+  name.forEach((item) => {
+    if (item.lastElementChild.textContent.toLowerCase().includes(inputValue)) {
+      item.parentElement.classList.remove("hidden");
+    } else {
+      item.parentElement.classList.add("hidden");
     }
-})
+  });
+});
 
-    
-})
-//get result and update UI
+// get result and update UI
 
 const updateUI = (data) => {
-   user.innerHTML = ''
+  user.innerHTML = "";
   data.forEach((item) => {
-  const {gender, name, picture, dob} = item
-  user.innerHTML = `
-              <li class="user__item">
+    const { gender, name, picture, location, dob } = item;
+
+    user.innerHTML += `
+     <li class="user__item">
               <button id="delete__btn" class="user__delete--btn">
                 <i class="fas fa-trash"></i>
               </button>
@@ -54,256 +53,31 @@ const updateUI = (data) => {
               />
               <div class="user__name">
                 <span class="material-symbols-outlined">badge</span>
-                <span>${name.title} ${name.first} ${name.lest}</span>
+                <span>- ${name.title} ${name.first} ${name.last}</span>
               </div>
               <div class="user__year">
                 <span class="material-symbols-outlined">cake</span>
-                <span>${dob.age}</span>
+                <span>- ${dob.age} years old.</span>
               </div>
               <div class="user__location">
                 <span class="material-symbols-outlined">person_pin_circle</span>
-                <span>${location.citty},${location.country}</span>
+                <span>-${location.city}, ${location.country}</span>
               </div>
               <div class="user__gender">
                 <span class="material-symbols-outlined">man</span>
-                <span>${gender}</span>
+                <span>- ${gender}</span>
               </div>
-            </li>
-            <li class="user__item">
-              <button id="delete__btn" class="user__delete--btn">
-                <i class="fas fa-trash"></i>
-              </button>
-              <img
-                class="user__img"
-                alt="User photo"
-                src="https://picsum.photos/id/338/400/400"
-                width="100"
-                height="100"
-              />
-              <div class="user__name">
-                <span class="material-symbols-outlined">badge</span>
-                <span>- Mr Akhror Soliev</span>
-              </div>
-              <div class="user__year">
-                <span class="material-symbols-outlined">cake</span>
-                <span>- 25 years old.</span>
-              </div>
-              <div class="user__location">
-                <span class="material-symbols-outlined">person_pin_circle</span>
-                <span>-Fergana, Uzbekistan</span>
-              </div>
-              <div class="user__gender">
-                <span class="material-symbols-outlined">man</span>
-                <span>- Male</span>
-              </div>
-            </li>
-            <li class="user__item">
-              <button id="delete__btn" class="user__delete--btn">
-                <i class="fas fa-trash"></i>
-              </button>
-              <img
-                class="user__img"
-                alt="User photo"
-                src="https://picsum.photos/id/338/400/400"
-                width="100"
-                height="100"
-              />
-              <div class="user__name">
-                <span class="material-symbols-outlined">badge</span>
-                <span>- Mr Akhror Soliev</span>
-              </div>
-              <div class="user__year">
-                <span class="material-symbols-outlined">cake</span>
-                <span>- 25 years old.</span>
-              </div>
-              <div class="user__location">
-                <span class="material-symbols-outlined">person_pin_circle</span>
-                <span>-Fergana, Uzbekistan</span>
-              </div>
-              <div class="user__gender">
-                <span class="material-symbols-outlined">man</span>
-                <span>- Male</span>
-              </div>
-            </li>
-            <li class="user__item">
-              <button id="delete__btn" class="user__delete--btn">
-                <i class="fas fa-trash"></i>
-              </button>
-              <img
-                class="user__img"
-                alt="User photo"
-                src="https://picsum.photos/id/338/400/400"
-                width="100"
-                height="100"
-              />
-              <div class="user__name">
-                <span class="material-symbols-outlined">badge</span>
-                <span>- Mr Akhror Soliev</span>
-              </div>
-              <div class="user__year">
-                <span class="material-symbols-outlined">cake</span>
-                <span>- 25 years old.</span>
-              </div>
-              <div class="user__location">
-                <span class="material-symbols-outlined">person_pin_circle</span>
-                <span>-Fergana, Uzbekistan</span>
-              </div>
-              <div class="user__gender">
-                <span class="material-symbols-outlined">man</span>
-                <span>- Male</span>
-              </div>
-            </li>
-            <li class="user__item">
-              <button id="delete__btn" class="user__delete--btn">
-                <i class="fas fa-trash"></i>
-              </button>
-              <img
-                class="user__img"
-                alt="User photo"
-                src="https://picsum.photos/id/338/400/400"
-                width="100"
-                height="100"
-              />
-              <div class="user__name">
-                <span class="material-symbols-outlined">badge</span>
-                <span>- Mr Akhror Soliev</span>
-              </div>
-              <div class="user__year">
-                <span class="material-symbols-outlined">cake</span>
-                <span>- 25 years old.</span>
-              </div>
-              <div class="user__location">
-                <span class="material-symbols-outlined">person_pin_circle</span>
-                <span>-Fergana, Uzbekistan</span>
-              </div>
-              <div class="user__gender">
-                <span class="material-symbols-outlined">man</span>
-                <span>- Male</span>
-              </div>
-            </li>
-            <li class="user__item">
-              <button id="delete__btn" class="user__delete--btn">
-                <i class="fas fa-trash"></i>
-              </button>
-              <img
-                class="user__img"
-                alt="User photo"
-                src="https://picsum.photos/id/338/400/400"
-                width="100"
-                height="100"
-              />
-              <div class="user__name">
-                <span class="material-symbols-outlined">badge</span>
-                <span>- Mr Akhror Soliev</span>
-              </div>
-              <div class="user__year">
-                <span class="material-symbols-outlined">cake</span>
-                <span>- 25 years old.</span>
-              </div>
-              <div class="user__location">
-                <span class="material-symbols-outlined">person_pin_circle</span>
-                <span>-Fergana, Uzbekistan</span>
-              </div>
-              <div class="user__gender">
-                <span class="material-symbols-outlined">man</span>
-                <span>- Male</span>
-              </div>
-            </li>
-            <li class="user__item">
-              <button id="delete__btn" class="user__delete--btn">
-                <i class="fas fa-trash"></i>
-              </button>
-              <img
-                class="user__img"
-                alt="User photo"
-                src="https://picsum.photos/id/338/400/400"
-                width="100"
-                height="100"
-              />
-              <div class="user__name">
-                <span class="material-symbols-outlined">badge</span>
-                <span>- Mr Akhror Soliev</span>
-              </div>
-              <div class="user__year">
-                <span class="material-symbols-outlined">cake</span>
-                <span>- 25 years old.</span>
-              </div>
-              <div class="user__location">
-                <span class="material-symbols-outlined">person_pin_circle</span>
-                <span>-Fergana, Uzbekistan</span>
-              </div>
-              <div class="user__gender">
-                <span class="material-symbols-outlined">man</span>
-                <span>- Male</span>
-              </div>
-            </li>
-            <li class="user__item">
-              <button id="delete__btn" class="user__delete--btn">
-                <i class="fas fa-trash"></i>
-              </button>
-              <img
-                class="user__img"
-                alt="User photo"
-                src="https://picsum.photos/id/338/400/400"
-                width="100"
-                height="100"
-              />
-              <div class="user__name">
-                <span class="material-symbols-outlined">badge</span>
-                <span>- Mr Akhror Soliev</span>
-              </div>
-              <div class="user__year">
-                <span class="material-symbols-outlined">cake</span>
-                <span>- 25 years old.</span>
-              </div>
-              <div class="user__location">
-                <span class="material-symbols-outlined">person_pin_circle</span>
-                <span>-Fergana, Uzbekistan</span>
-              </div>
-              <div class="user__gender">
-                <span class="material-symbols-outlined">man</span>
-                <span>- Male</span>
-              </div>
-            </li>
-            <li class="user__item">
-              <button id="delete__btn" class="user__delete--btn">
-                <i class="fas fa-trash"></i>
-              </button>
-              <img
-                class="user__img"
-                alt="User photo"
-                src="https://picsum.photos/id/338/400/400"
-                width="100"
-                height="100"
-              />
-              <div class="user__name">
-                <span class="material-symbols-outlined">badge</span>
-                <span>- Mr Akhror Soliev</span>
-              </div>
-              <div class="user__year">
-                <span class="material-symbols-outlined">cake</span>
-                <span>- 25 years old.</span>
-              </div>
-              <div class="user__location">
-                <span class="material-symbols-outlined">person_pin_circle</span>
-                <span>-Fergana, Uzbekistan</span>
-              </div>
-              <div class="user__gender">
-                <span class="material-symbols-outlined">man</span>
-                <span>- Male</span>
-              </div>
-            </li>
-  `
-  }) 
-}
-// delete user 
-document.addEventListener('click', (e) => {
-    if(e.target.classList[0] === 'user__delete--btn'){
-        e.target.parentElement.remove()
-    }
-   
-    
-    if (!user.children.length) {
-        clearBtn.classList.add('hidden')
-    }
-})
+            </li>`;
+  });
+};
+
+// delete user
+document.addEventListener("click", (e) => {
+  if (e.target.classList[0] === "user__delete--btn") {
+    e.target.parentElement.remove();
+  }
+
+  if (!user.children.length) {
+    clearBtn.classList.add("hidden");
+  }
+});
